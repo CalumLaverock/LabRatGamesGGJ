34586c5f44341af932eccf9b7eedb85423ffa3b2
Frazzle17 <59959787+Frazzle17@users.noreply.github.com>
2021-01-29 21:47:25 +0000

    Created Tilesets
    Separated tile sprites into 9 seperate sprites; turned sprites into tile objects; added tiles to tile pallette; added custom physics shape to tile objects; added tilmap/composite collider to tilemap

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+
+    public bool dead = false;
+    public float speed = 5;
+
+    Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if(dead == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + (speed * horizontal * Time.deltaTime);
+        position.y = position.y + (speed * vertical * Time.deltaTime);
+
+        rigidbody2d.MovePosition(position);
+    }
+}
